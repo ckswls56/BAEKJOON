15a605b8fe197edd882d23efe17e0efbaf867f7d
Nachanjin <skxkswls@gmail.com>
2022-06-27 02:51:20 +0900

    MtoN

---
+++ 15/1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int idx = 0;
+int promissing(char *arr,int n) {
+	for (int j = idx; j > 0; j--) {
+		if (arr[j - 1]-'0' == n)
+			return 0;
+	}
+	return 1;
+}
+
+void ntom(int n, int m, char *arr) {
+
+	
+	if (m == 0) {
+		
+		for(int i=0;i<idx;i++)
+			printf("%c ",arr[i]);
+		printf("\n");
+		return;
+	}
+	int flag = 1;
+	for (int i = 1; i <= n; i++) {
+
+		if (promissing(arr, i)) {
+			arr[idx++] = '0' + i;
+			ntom(n, m - 1, arr);
+
