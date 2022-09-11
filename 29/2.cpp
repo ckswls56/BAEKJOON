57659796e20fb2d8724778a6cb3c75081d09fb36
Nachanjin <skxkswls@gmail.com>
2022-09-11 18:47:02 +0900

    tree radious

--- 29/2.cpp
+++ 29/2.cpp
@@ -1,68 +1,53 @@
 #include <iostream>
-#include <queue>
-#include <stack>
+#include <cstring>
 #include <vector>
 #include <algorithm>
 #define MAX_VERTEX 100000 + 1
 using namespace std;
 
 vector<pair<int, int>> graph[MAX_VERTEX];
-int res;
-
-struct comp
+bool visit[MAX_VERTEX];
+int max_dist, max_node;
+void dfs(int node, int dist)
 {
-    bool operator()(pair<int, int> a, pair<int, int> b)
-    {
-        return a.second > b.second;
-    }
-};
+    if (visit[node])
+        return;
 
-int dfs(int node, int b)
-{
-    if (graph[node].size() == 0)
+    if (max_dist < dist)
     {
-        return b;
+        max_dist = dist;
+        max_node = node;
     }
-    priority_queue<int> q;
-    int leaf1, leaf2;
-    int max_leaf = 0;
+    visit[node] = true;
+
     for (int i = 0; i < graph[node].size(); i++)
     {
-        int leaf = dfs(graph[node][i].first, graph[node][i].second);
-        max_leaf = max(max_leaf, leaf);
-        q.push(leaf);
+        int next = graph[node][i].first;
+        int next_dist = graph[node][i].second;
+        dfs(next, dist + next_dist);
     }
-
-    leaf1 = q.top();
-    q.pop();
-    if (!q.empty())
-        leaf2 = q.top();
-    else
-        leaf2 = 0;
-
-    if (leaf1 + leaf2 > res)
-    {
-        res = leaf1 + leaf2;
-    }
-
-    return leaf1 + b;
 }
 
 int main()
-{
+{ // 트리의 지름은 임의의 한정점 에서 가장먼 v1를 찾고 v1에서 가장 먼 v2를 찾아 거리 출력
     int n, u, v, w;
     scanf("%d", &n);
     while (n--)
     {
         scanf("%d %d", &u, &v);
-        v = 0;
         while (v != -1)
         {
             scanf("%d", &w);
             graph[u].push_back({v, w});
+            graph[v].push_back({u, w});
             scanf("%d", &v);
         }
     }
-    res = max(res, dfs(1, 0));
-    printf("%d\n", res);
+    dfs(1, 0);
+    memset(visit, 0, sizeof(visit));
+    max_dist = 0;
+
+    dfs(max_node, 0);
+
+    printf("%d\n", max_dist);
 }
